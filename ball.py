62cf8d663bb790874b4bcdbe5ae9e4205efe64cc
Henry839 <2000017754@stu.pku.edu.cn>
2023-06-25 15:04:48 +0800

    initial velocity set and bouncing velocity set

--- ball.py
+++ ball.py
@@ -19,7 +19,8 @@ class Ball(pygame.sprite.Sprite):
         # Draw the ball (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         
-        self.velocity = [randint(4,8),randint(-8,8)]
+#        self.velocity = [randint(4,8),randint(-8,8)]
+        self.velocity = [4,4]
         
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
@@ -29,5 +30,5 @@ class Ball(pygame.sprite.Sprite):
         self.rect.y += self.velocity[1]
           
     def bounce(self):
-        self.velocity[0] = -self.velocity[0]
-        self.velocity[1] = randint(-8,8)
+        self.velocity[0] = self.velocity[0]
+        self.velocity[1] = -self.velocity[1]
