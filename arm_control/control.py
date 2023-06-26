26058eca56c93e650832f0a9b2d031c5acaaae38
Henry839 <2000017754@stu.pku.edu.cn>
2023-06-26 14:17:45 +0800

    inverse kinematics done!

--- arm_control/control.py
+++ arm_control/control.py
@@ -1,27 +1,35 @@
 import math
-from math import sqrt, asin, acos
+from math import sqrt, asin, acos, atan2, sin, cos
 class arm_controller():
     def __init__(self, arm1_length, arm2_length, x_origin, y_origin):
 
         # x_origin: x coordinate of the base of arm
         # y_origin: y coordinate of the base of arm
-        self.arm1_length = arm1_length
-        self.arm2_length = arm2_length
+        self.l1 = arm1_length
+        self.l2 = arm2_length
         self.x_origin = x_origin
         self.y_origin = y_origin
 
     def inverse_kinematics(self, x_paddle, y_paddle):
         # x_paddle: x coordinate of target place of the paddle's center
         # y_paddle: y coordinate of target place of the paddle's center
-        length = sqrt(pow(x_paddle - self.x_origin,2) + pow(y_paddle - self.y_origin,2))
-        if(length != (self.arm1_length + self.arm2_length)):
-            print("ERROR: x_paddle and y_paddle error")
-            raise ValueError
-
+        # return alpha1, alpha2(list)
         # calculate the angle
-        alpha = acos((x_paddle - self.x_origin)/length)
-        alpha = math.degrees(alpha)
-        return alpha
+        x = x_paddle - self.x_origin
+        y = -(y_paddle - self.y_origin)
+        length = sqrt(x**2 + y**2)
+        l1 = self.l1
+        l2 = self.l2
+        if l1 + l2 == length:
+            f = 0
+        else:
+            f = acos((x**2 + y**2 - l1**2 -l2**2) / 2*l1*l2)
+        f1 = (l1**2 + (x**2 + y**2) - l2**2)/(2*l1*sqrt(x**2 + y**2))
+        alpha1 = [math.degrees(atan2(y,x) - acos(f1)), math.degrees(atan2(y,x) + acos(f1))]
+        alpha2 = [alpha1[0] + math.degrees(f),alpha1[1] + math.degrees(-f)]
+
+
+        return alpha1, alpha2
         
 
         
